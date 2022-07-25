c6c7509c62e874c92df32dc8725caf686c2d96da
Sb-Rahman <purnimajahan2@gmail.com>
2022-07-25 09:52:26 +0600

    Android seekbar

---
+++ app/src/main/java/com/bipul/seekbar/MainActivity.java
@@ -0,0 +1,44 @@
+package com.bipul.seekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //declar global variables
+    SeekBar seekBar;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//initialize  variables
+        seekBar = findViewById(R.id.seekid);
+        textView = findViewById(R.id.valueshow);
+
+
+        //setAction
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+//                Toast.makeText(MainActivity.this, "Your process is " + i, Toast.LENGTH_SHORT).show();
+                textView.setText("Your Process is: " + i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
